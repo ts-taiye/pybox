42206fe83197bfccaa1f58374bb2d4d534054919
v_tsander <v_tsander@wargaming.net>
2018-11-10 23:10:04 +0300

    changed type annotations to use T instead of IService

--- pybox/inject.py
+++ pybox/inject.py
@@ -1,6 +1,6 @@
 from typing import Any, Optional, Type
 
-from pybox.service import Container, IService
+from pybox.service import Container
 from pybox.utils import T
 
 
@@ -20,20 +20,20 @@ class InstanceIsRequiredException(Exception):
 
 
 class InjectLazy:
-    _dependency: Type[IService]
+    _dependency: Type[T]
     _attribute_name: Optional[str]
 
-    def __init__(self, dependency: Type[IService]):
+    def __init__(self, dependency: Type[T]):
         self._dependency = dependency
         self._attribute_name = None
 
-    def __set__(self, instance: IService, value: Any):
+    def __set__(self, instance: T, value: Any):
         raise SetOperationIsNotPermittedException()
 
-    def __set_name__(self, owner: Type[IService], name: str):
+    def __set_name__(self, owner: Type[T], name: str):
         self._attribute_name = f'__di_{name}__'
 
-    def __get__(self, instance: Optional[IService], owner: Type[IService]) -> IService:
+    def __get__(self, instance: Optional[T], owner: Type[T]) -> T:
         if instance is None:
             raise InstanceIsRequiredException()
         if self._attribute_name not in instance.__dict__:
