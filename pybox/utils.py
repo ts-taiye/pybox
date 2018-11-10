fbf1c9a1b262d079b8ce351ef3eccd9d615f7c07
v_tsander <v_tsander@wargaming.net>
2018-11-10 18:41:47 +0300

    Fixed typos, added types

--- pybox/utils.py
+++ pybox/utils.py
@@ -1,14 +1,20 @@
+from typing import Type, TypeVar
+
 __all__ = (
     'Singleton',
+    'T'
 )
 
 
+T = TypeVar('T')
+
+
 class Singleton:
-    def __init__(self, class_object):
+    def __init__(self, class_object: Type[T]) -> None:
         self.class_object = class_object
         self.instance = None
 
-    def __call__(self,*args,**kwds):
+    def __call__(self, *args, **kwargs) -> T:
         if self.instance is None:
-            self.instance = self.class_object(*args,**kwds)
+            self.instance = self.class_object(*args, **kwargs)
         return self.instance
